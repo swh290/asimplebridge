5c08947ffe6d85539945b86c4bb9c783511de12b
chiayenhung <hodalalala@gmail.com>
2013-09-08 23:01:19 -0400

    Add account app and register in admin

--- account/models.py
+++ account/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 # Create your models here.
 class UserProfile(models.Model):
       user = models.OneToOneField( User, unique=True )
+
+admin.site.register(UserProfile)
