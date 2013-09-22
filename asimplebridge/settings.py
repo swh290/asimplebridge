68358fed80e4821ab4e8013c55819fe8cd00bb54
chiayenhung <hodalalala@gmail.com>
2013-09-22 16:48:36 -0400

    change static file path

--- asimplebridge/settings.py
+++ asimplebridge/settings.py
@@ -69,14 +69,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT= os.path.join(PROJECT_PATH,'staticfiles/')
+STATIC_ROOT= '/staticfiles/'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, "asimplebridge/static/"),
-    os.path.join(PROJECT_PATH, "static/"),
+    # os.path.join(PROJECT_PATH, "asimplebridge/static"),
+    os.path.join(PROJECT_PATH, "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -87,7 +87,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
