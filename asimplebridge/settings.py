762e38e15d98e0801dfa7d84ab7b9fb6b71f2823
chiayenhung <hodalalala@gmail.com>
2013-09-22 16:40:37 -0400

    static to static/

--- asimplebridge/settings.py
+++ asimplebridge/settings.py
@@ -75,8 +75,8 @@ STATIC_ROOT= os.path.join(PROJECT_PATH,'staticfiles/')
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, "asimplebridge/static"),
-    os.path.join(PROJECT_PATH, "static"),
+    os.path.join(PROJECT_PATH, "asimplebridge/static/"),
+    os.path.join(PROJECT_PATH, "static/"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
