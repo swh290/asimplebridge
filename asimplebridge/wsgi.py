3aebb62c9f340ffa0bfab08c9cc3a3a501f2bb55
chiayenhung <hodalalala@gmail.com>
2013-09-22 21:40:18 -0400

    change wsgi

--- asimplebridge/wsgi.py
+++ asimplebridge/wsgi.py
@@ -14,7 +14,7 @@ framework.
 
 """
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from dj_static import Cling
 
 import os
 
@@ -27,4 +27,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asimplebridge.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+application = get_wsgi_application()
+# application = Cling(get_wsgi_application())
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 application = Cling(get_wsgi_application())
